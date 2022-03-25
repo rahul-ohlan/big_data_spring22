ff4376686a7e4c65a277b47206d88b5639acd85b
rahul-ohlan <ohlanrahul1000@gmail.com>
2022-03-25 13:42:45 -0400

    updated mappe and reducer for parking data

--- K_Means_project_1/mapper.py
+++ K_Means_project_1/mapper.py
@@ -9,10 +9,7 @@ f_handle = open('Parking_Violations_Issued_-_Fiscal_Year_2022.csv')
 
 c = 0
 data = f_handle.readlines()
-# data = iter(data)
-# next(data)
-# year_pat = re.compile(r'^\d\d\d\d$')
-# body_type_pat = re.compile(r'^[a-zA-Z]+\S$')
+
 for line in data:
     line = line.strip()
     line = line.split(',')
@@ -20,7 +17,7 @@ for line in data:
     year = str(line[35])
 
     if not (year!= '' and year!="Vehicle Year" and (int(year) > 1900 and int(year) < 2023)):
-        continue           # skipping rows with invalid registration year data
+        continue           # skipping rows with invalid registration year data or missing values
 
     bodyType = str(line[6])
 
@@ -36,23 +33,5 @@ for line in data:
         break
 
     c+=1
-    # try:
-    #     reg_year = year_pat.search(line[35])
-    # except:
-    #     reg_year = "None"
-
-    # if reg_year:
-    #     reg_year = reg_year[0]
-    # body_type = body_type_pat.search(line[6])
-
-    # if  body_type and line[6]!="Vehicle Body Type":
-    #     body_type = body_type[0]
-    # else:
-    #     body_type = "None"
-
-    # if c==3:
-    #     break
-    # print(reg_year+"\t"+body_type)
-
 
 f_handle.close()
--- K_Means_project_1/reducer.py
+++ K_Means_project_1/reducer.py
@@ -12,28 +12,20 @@ type_dict = dict()
 for line in sys.stdin:
 
     line = line.strip()
-    year = line.split('_')[0]
-    b_type = line.split('_')[1].split('\t')[0]
-    num = line.split('_')[1].split('\t')[1]
+    line = line.split()
+    year = line[0].split('_')[0]
+    b_type = line[0].split('_')[1]
+    num = line[1]
     num = int(num)
 
     year_dict[year] = year_dict.get(year,0) + num
     type_dict[b_type] = type_dict.get(b_type,0) + num
 
 
-sorted_years = sorted(year_dict, key = itemgetter(1),reverse=True)
-sorted_types = sorted(type_dict, key = itemgetter(1),reverse=True)
+sorted_years = sorted(year_dict.items(), key = itemgetter(1),reverse=True)
+sorted_types = sorted(type_dict.items(), key = itemgetter(1),reverse=True)
 
 print('top 5 years: ',end=' ')
 print(sorted_years[0:5])
 print('top 5 car types: ',end=' ')
 print(sorted_types[0:5])
-
-
-
-
-
-    # print(year,b_type)
-
-    # print(line,end='')
-
